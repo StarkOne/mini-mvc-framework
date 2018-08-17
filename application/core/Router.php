2209ea5013e2153add7bc9d66e26129e858a1ecf
StarkOne <Stark.pro40@gmail.com>
2018-08-17 14:27:13 +0700

    Router

--- application/core/Router.php
+++ application/core/Router.php
@@ -9,20 +9,42 @@ class Router
 
   public function __construct()
   {
-    
+    $arr = require_once 'application/config/routes.php';
+    foreach ($arr as $key => $value) {
+      $this->add($key, $value);
+    }
   }
 
-  public function add()
+  public function add($route, $params)
   {
-
+    $route = '#^'. $route .'#';
+    $this->router[$route] = $params;
   }
+
   public function match()
   {
-
+    $url = trim($_SERVER['REQUEST_URI'], '/');
+    foreach ($this->router as $route => $parems) {
+      if(preg_match($route, $url, $matches)) {
+        $this->params = $parems;
+        return true;
+      }
+    }
+    return false;
   }
+
   public function run()
   {
-    echo 'Start';
+    if($this->match()) {
+      $controller = 'application\controllers\\' . ucfirst($this->params['controller']) . 'Controller.php';
+      if(class_exists($controller)) {
+        echo 'ОК';
+      } else {
+        echo 'Не найден' . $controller;
+      }
+    } else {
+      echo "Маршрут не найден";
+    }
   }
 
 }
